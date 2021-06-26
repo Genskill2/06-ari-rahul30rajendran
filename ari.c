fb1000a4a7e180177b23d0121c3b04f084fcd670
rahul30rajendran <84763801+rahul30rajendran@users.noreply.github.com>
2021-06-26 13:55:12 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -14,7 +14,7 @@ int words=1,sentences=0;
 for(int i=0;i<strlen(s);i++)
 {if(isalnum(s)==1)
 letters++;
-if((s[i]=='.'&&s[i+1]==0)||s[i]=='?'||s[i]=='!'){
+if((s[i]=='.'||s[i]=='?'||s[i]=='!'){
 sentences++;
 }
 if(s[i]==' ')
